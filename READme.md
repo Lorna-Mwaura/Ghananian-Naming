ace0d37a2a318ee9e61313d0f9d47767e157658e
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-03-06 22:35:40 +0300

    st. style with css

---
+++ READme.md
@@ -0,0 +1,31 @@
+# Title
+## Single--Landing--Page
+
+# Table of content
+* Description
+* Installation Requirements
+* Technologies used
+* License
+* Authors Info
+
+
+## Description
+This is a simple single landing page on a travel company where I have included the companies location, contact and what the company offers.
+
+## Setup/Installation Requirements
+* Make sure you have access to a browser
+* Click on the live link in the About section
+or click on this live link  https://lorna-mwaura.github.io/Single--Landing--Page/ 
+
+
+## Technologies Used
+Languages used are HTML and Css.
+
+### License
+Copyright (c) 2022 [Click to view license](LICENSE)
+
+### Author info 
+## By Lorna-Mwaura
+## lornamwaura99@gmail.com
+
+
--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-// function akanName() {
-//     let year = document.getElementbyId("year");
-//     let month = document.getElementbyId("month");
-//     let date = document.getElementbyId("date");
-//     let btn = document.getElementById("contact.submit");
-// }
+let year = document.getElementById("year-input");
+let month = document.getElementById("month-input");
+let date = document.getElementById("day-input");
+let gender = document.userdata.gender.value;
+
+function akanName() {
+   
+    calculateBirthDate ()
+    // let btn = document.getElementById("contact.submit");
+}
+
 
 function calculateBirthDate() {
-    let  d = Math.floor
-    ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-    let CC = year.value.str.substring(0, 2);
-    let YY = year.value.str.substring(2);
+    let CC = year.value.substring(0, 2);
+    let YY = year.value.substring(2);
     let MM = month.value;
     let DD = date.value;
+    let  d = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+    console.log (d)
+
   return d;
 }
 // const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 // function gender(maleNames){
 // return maleNames ("Your Akan name is" + femaleNames);
 //  } 
-console.log("Hey my year is" + year.value)
+// console.log("Hey my year is" + year.value)
